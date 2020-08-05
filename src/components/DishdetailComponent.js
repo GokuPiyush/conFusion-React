14bf949b94d1edd7d6d76cdfba64c4d59e59b161
Piyush Yadav <PiyushTheGoku@gamil.com>
2020-08-05 12:27:53 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -18,7 +18,7 @@ class DishDetail extends Component{
             const comment = comments.map((comment) => {
                 return (
                     <li>
-                        {comment.comment}<br />--{comment.author}, {comment.date}
+                        {comment.comment}<br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                     </li>
                 );
             });
@@ -37,12 +37,14 @@ class DishDetail extends Component{
     render(){
         if(this.props.dish != null){
             return(
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div  className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
+                <div className="container">
+                    <div className="row">
+                        <div  className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div  className="col-12 col-md-5 m-1">
+                            {this.renderComments(this.props.dish.comments)}
+                        </div>
                     </div>
                 </div>
             );
